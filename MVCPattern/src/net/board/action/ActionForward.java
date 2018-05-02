7564fb290bd2e393def6a0018721e56b7e4c525c
CoRock <mikecorock@gmail.com>
2018-05-02 11:09:28 +0900

    mvc project

---
+++ MVCPattern/src/net/board/action/ActionForward.java
@@ -0,0 +1,22 @@
+package net.board.action;
+
+public class ActionForward {
+	
+	// 처음에는 무조건 forward 방식으로 하겠다!
+	private boolean isRedirect = false;
+	private String path = null;
+	
+	public boolean isRedirect() {
+		return isRedirect;
+	}
+	public void setRedirect(boolean isRedirect) {
+		this.isRedirect = isRedirect;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	
+}
